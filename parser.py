dee5d094b22f78d562a8673a248d0151e676994c
Steven Marshall <DigitalSenseDesign@gmail.com>
2019-03-19 00:47:40 +0000

    Added ability to sanitise strings using translation class.
    Translator removes:
    - Empty string.
    - Strings with only alpha characters.
    - Miscellaneous symbols.
    
    Added test function to handle common string modification and checks.

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import pyparsing as pp
+import string
 
 class Parser:
 
@@ -27,8 +28,37 @@ class Parser:
             print('     No Match')
             #print$('     No Match : {}\n'.format(str(parse_exception)))
 
+class Translator:
+
+    def __init__(self):
+        self.translation_Table = self.__generate_table()
+
+    def __generate_table(self):
+        return str.maketrans('', '', '!"£$%^&()_={}[]:;\'\\@#~<>,.?|')
+
+    def remove_symbols(self, string):
+        return string.translate(self.translation_Table)
+
+    def filter(self, string):
+        filtered = list()
+        for string in string.split():
+            if string.isalpha():
+                print('---- Word Found : {}'.format(string))
+                continue
+            else:
+                filtered.append(string)
+        return filtered
+
+def test(string):
+    print('---- Input String : {}'.format(string))
+    alphanum = Translator.remove_symbols(string)
+    filtered = Translator.filter(alphanum)
+    for string in filtered:
+        Parser.parse_string(string)
+
 if __name__ == '__main__':
     Parser = Parser()
+    Translator = Translator()
 
     test_rolls = [
         #Fail
@@ -36,10 +66,8 @@ if __name__ == '__main__':
         #Pass
         '5+5', 'd6', '1d6', 'd1000/10', 'd6*4', 'd6-d6', 'd6+5', 'd6+d6', 'd6+1d6', '1d6+1', '1d6+d6', '1d6+1d6', '3d12+2/1d2', '2d6+3d6-4d6/5d6*6d6'
     ]
-
     for roll in test_rolls:
-        Parser.parse_string(roll)
+        test(roll)
 
     test_string = 'This is a test of the parsers ability to seperate rolls from strings of text, for example 1d6 should be parsed and 0d5 should not.'
-    
-    Parser.parse_string(test_string)
+    test(test_string)
